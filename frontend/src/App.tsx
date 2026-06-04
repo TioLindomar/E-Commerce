9fa0b23cd0a1f037c3a4d77ed9bc16f1eecec3a8
Tio Lindomar <mateusxavier0707@gmail.com>
2026-06-04 19:29:11 -0300

    chore: cria a estrutura inicial do projeto
    Cria a estrutura inicial do projeto com duas pastas centrais na raiz: frontend e backend.
    O frontend foi estruturado com React + Vite e foram configuradas as bibliotecas tailwindcss, shadcn/ui Lucide Icons e Zustand.
    Arquivo PADROES-DE-COMMIT.md modificado para agilizar a leitura.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <section
+        id="center"
+        className="bg-accent-foreground flex flex-col items-center justify-center h-screen text-white"
+      >
+        <h1>Início</h1>
+        <p>Olá, Mundo</p>
+      </section>
+    </>
+  );
+}
+
+export default App;
