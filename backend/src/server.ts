838457c77c174505b89964183ad61f9b9868a131
Tio Lindomar <mateusxavier0707@gmail.com>
2026-07-10 17:08:47 -0300

    fix: tentativa de fazer o Github Pages funcionar

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -24,54 +24,54 @@ app.use(express.json());
 app.use(apiRoutes);
 app.use(errorMiddleware);
 
-app.get("/", (req, res) => {
-	res.send("Olá, Mundo!");
-});
+// app.get("/", (req, res) => {
+// 	res.send("Olá, Mundo!");
+// });
 
-app.get("/api", (req, res) => {
-	res.send({ roupas: ["Camiseta", "Calça cargo", "Boné"] });
-});
+// app.get("/api", (req, res) => {
+// 	res.send({ roupas: ["Camiseta", "Calça cargo", "Boné"] });
+// });
 
-// * Retornar lista de produtos
-app.get("/products", (req, res) => {
-	res.json([
-		{ id: 1, name: "T-Shirt", price: 59.99 },
-		{ id: 2, name: "Cargo Pants", price: 120.0 },
-		{ id: 3, name: "Cap", price: 35.5 },
-		{ id: 4, name: "Hoodie", price: 150.0 },
-		{ id: 5, name: "Sneakers", price: 299.9 },
-		{ id: 6, name: "Socks", price: 15.0 },
-		{ id: 7, name: "Jacket", price: 250.0 },
-		{ id: 8, name: "Shorts", price: 45.0 },
-		{ id: 9, name: "Belt", price: 25.0 },
-		{ id: 10, name: "Backpack", price: 180.0 },
-	]);
-});
+// // * Retornar lista de produtos
+// app.get("/products", (req, res) => {
+// 	res.json([
+// 		{ id: 1, name: "T-Shirt", price: 59.99 },
+// 		{ id: 2, name: "Cargo Pants", price: 120.0 },
+// 		{ id: 3, name: "Cap", price: 35.5 },
+// 		{ id: 4, name: "Hoodie", price: 150.0 },
+// 		{ id: 5, name: "Sneakers", price: 299.9 },
+// 		{ id: 6, name: "Socks", price: 15.0 },
+// 		{ id: 7, name: "Jacket", price: 250.0 },
+// 		{ id: 8, name: "Shorts", price: 45.0 },
+// 		{ id: 9, name: "Belt", price: 25.0 },
+// 		{ id: 10, name: "Backpack", price: 180.0 },
+// 	]);
+// });
 
-app.get("/products/:id", (req, res) => {
-	const id = Number(req.params.id);
-	const products = [
-		{ id: 1, name: "T-Shirt", price: 59.99 },
-		{ id: 2, name: "Cargo Pants", price: 120.0 },
-		{ id: 3, name: "Cap", price: 35.5 },
-		{ id: 4, name: "Hoodie", price: 150.0 },
-		{ id: 5, name: "Sneakers", price: 299.9 },
-		{ id: 6, name: "Socks", price: 15.0 },
-		{ id: 7, name: "Jacket", price: 250.0 },
-		{ id: 8, name: "Shorts", price: 45.0 },
-		{ id: 9, name: "Belt", price: 25.0 },
-		{ id: 10, name: "Backpack", price: 180.0 },
-	];
+// app.get("/products/:id", (req, res) => {
+// 	const id = Number(req.params.id);
+// 	const products = [
+// 		{ id: 1, name: "T-Shirt", price: 59.99 },
+// 		{ id: 2, name: "Cargo Pants", price: 120.0 },
+// 		{ id: 3, name: "Cap", price: 35.5 },
+// 		{ id: 4, name: "Hoodie", price: 150.0 },
+// 		{ id: 5, name: "Sneakers", price: 299.9 },
+// 		{ id: 6, name: "Socks", price: 15.0 },
+// 		{ id: 7, name: "Jacket", price: 250.0 },
+// 		{ id: 8, name: "Shorts", price: 45.0 },
+// 		{ id: 9, name: "Belt", price: 25.0 },
+// 		{ id: 10, name: "Backpack", price: 180.0 },
+// 	];
 
-	const requestedProduct = products.find((product) => product.id === id);
+// 	const requestedProduct = products.find((product) => product.id === id);
 
-	// Retorna 404 se a busca falhar
-	if (!requestedProduct) {
-		return res.status(404).json({ error: "Produto não encontrado." });
-	}
+// 	// Retorna 404 se a busca falhar
+// 	if (!requestedProduct) {
+// 		return res.status(404).json({ error: "Produto não encontrado." });
+// 	}
 
-	res.json(requestedProduct);
-});
+// 	res.json(requestedProduct);
+// });
 
 app.listen(PORT, () => {
 	console.log(`Servidor rodando na porta ${PORT}`);
